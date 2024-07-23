1b17a8d26aed1a2a009c4944659531d900ec0d94
jirishus <jirishus@yahoo.com>
2024-07-22 21:00:17 -0700

    render html public file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +10,13 @@ app.use(express.json());
 app.get('/api/data', (req, res) => {
     console.log('Processing GET Request');
     res.json({ message: 'Hello from Express!' });
+});
+
+app.get('/page', (req, res) => {
+    fs.readFile(__dirname + '/public/index.html', 'utf8', (err, text) => {
+        if (err) console.log(err);
+        res.send(text);
+    });
 });
 
 app.listen(port, () => {
